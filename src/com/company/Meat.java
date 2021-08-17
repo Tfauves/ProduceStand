dcee6983ca01350cc10bce5a07ba2833c5b80526
Tfauves <tfauvelle@gmail.com>
2021-08-17 18:25:18 -0400

    added console class.

--- src/com/company/Meat.java
+++ src/com/company/Meat.java
@@ -6,4 +6,6 @@ public class Meat extends Product{
     public Meat(String category, String inventoryId, String name, double pricePerPound, int qtyAvailable) {
         super(category, inventoryId, name, pricePerPound, qtyAvailable);
     }
+
+
 }
--- src/com/company/Veggies.java
+++ src/com/company/Veggies.java
@@ -6,4 +6,6 @@ public class Veggies extends Product{
     public Veggies(String category, String inventoryId, String name, double pricePerPound, int qtyAvailable) {
         super(category, inventoryId, name, pricePerPound, qtyAvailable);
     }
+
+
 }
