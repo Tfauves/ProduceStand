5efb063f4df5ff2a6c22965c69f357c1e1ecad9d
unknown <tfauvelle@gmail.com>
2021-08-17 14:44:43 -0500

    inventory class and fruit class.

---
+++ src/com/company/Inventory.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory {
+   public List<Product> inventory = new ArrayList<>();
+   public List<Product> reOrder = new ArrayList<>();
+
+    public void addToInventory(Product product){
+
+    }
+
+    public void removeFromInventory() {
+
+    }
+
+
+}
--- src/com/company/Product.java
+++ src/com/company/Product.java
@@ -1,5 +1,19 @@
 package com.company;
 
 public class Product {
+    private String category;
+    private String inventoryId;
+    private String name;
+    private double pricePerPound;
+    private int qtyAvailable;
+
+    public Product(String category, String inventoryId, String name, double pricePerPound, int qtyAvailable) {
+        this.category = category;
+        this.inventoryId = inventoryId;
+        this.name = name;
+        this.pricePerPound = pricePerPound;
+        this.qtyAvailable = qtyAvailable;
+    }
+
 
 }
